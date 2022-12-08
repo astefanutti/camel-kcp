1e9dc6df105b922bb6b716823ac6e7ae9000d500
Antonin Stefanutti <antonin@stefanutti.fr>
2022-12-08 15:17:33 +0100

    test: Set Gomega defaults

---
+++ test/support/gomega.go
@@ -0,0 +1,45 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package support
+
+import (
+	"time"
+
+	"github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
+	"github.com/onsi/gomega/gstruct"
+	"github.com/onsi/gomega/types"
+)
+
+func init() {
+	// Gomega settings
+	gomega.SetDefaultEventuallyTimeout(TestTimeoutShort)
+	gomega.SetDefaultEventuallyPollingInterval(1 * time.Second)
+	gomega.SetDefaultConsistentlyDuration(30 * time.Second)
+	gomega.SetDefaultConsistentlyPollingInterval(1 * time.Second)
+	// Disable object truncation on test results
+	format.MaxLength = 0
+}
+
+func EqualP(expected interface{}) types.GomegaMatcher {
+	return gstruct.PointTo(gomega.Equal(expected))
+}
+
+func MatchFieldsP(options gstruct.Options, fields gstruct.Fields) types.GomegaMatcher {
+	return gstruct.PointTo(gstruct.MatchFields(options, fields))
+}
