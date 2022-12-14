bb42d4575c11cdee60f6aa5a13650030d43f0ed3
Antonin Stefanutti <antonin@stefanutti.fr>
2022-12-14 15:42:42 +0100

    lint: fix golint error

--- test/support/client.go
+++ test/support/client.go
@@ -19,6 +19,8 @@ package support
 
 import (
 	"k8s.io/client-go/kubernetes"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
