3acf8ef75dee6168aee60c30992a7a211acdcf95
Antonin Stefanutti <antonin@stefanutti.fr>
2023-03-16 14:09:08 +0100

    test: Fix lint errors

--- test/support/client.go
+++ test/support/client.go
@@ -18,7 +18,6 @@ limitations under the License.
 package support
 
 import (
-
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
